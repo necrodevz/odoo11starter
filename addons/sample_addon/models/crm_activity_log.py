d4a8f721a276f975eeba6ee34536040bc8c5ed35
Devz <necrodevz@gmail.com>
2018-01-05 19:51:15 -0500

    Init

--- addons/sample_addon/models/crm_activity_log.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class Technichian (models.Model):
-    _name = 'crm.activity.log'
-    _inherit='crm.activty.log'
-
-    technician_id = fields.Many2one('hr.employees', string="Technician")
